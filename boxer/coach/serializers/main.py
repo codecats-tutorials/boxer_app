9e125f9bce8e2239340a4c3f3e868913a2b47c34
ssstrz <codecats@gmail.com>
2015-04-15 22:59:17 +0200

    added serialization

---
+++ boxer/coach/serializers/main.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from coach.documents import Coach
+from player.serializers.main import PlayerSerializer
+
+__author__ = 't'
+
+class CoachSerializer(serializers.Serializer):
+    id = serializers.CharField(required=False)
+    name = serializers.CharField()
+    surname = serializers.CharField()
+    rate = serializers.IntegerField(min_value=0, max_value=5, required=False)
+    players = serializers.ListField()
+
+    def __init__(self, *args, **kwargs):
+        if 'data' in kwargs:
+            kwargs['data']['players'] = kwargs['data'].get('selectedPlayers')
+
+        super(CoachSerializer, self).__init__(*args, **kwargs)
+
+    def update(self, instance, validated_data):
+        for v in validated_data:
+            setattr(instance, v, validated_data.get(v))
+        return instance
+
+    def create(self, validated_data):
+        return Coach(**validated_data)
+
+    def to_representation(self, instance):
+        represent = super(CoachSerializer, self).to_representation(instance)
+        data = map(lambda x: PlayerSerializer(x).data, represent['players'])
+        represent['players'] = data
+        return represent
--- boxer/coach/views/main.py
+++ boxer/coach/views/main.py
@@ -1,16 +1,26 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
-from player.documents import Player
-from player.serializers.main import PlayerSerializer
+from coach.documents import Coach
+from coach.serializers.main import CoachSerializer
 
 __author__ = 't'
 
 class Main(APIView):
     def get(self, request, *args, **kwargs):
-        return JsonResponse({})
+        page = int(request.GET.get('page', 1))
+        coaches = Coach.objects()[(page-1) * 5: page * 5]
+        return JsonResponse({'data': map(lambda x: CoachSerializer(x).data, coaches)})
 
     def post(self, request, *args, **kwargs):
-        pass
+        data = request.DATA.copy()
+        serializer = CoachSerializer(data=data)
+        if serializer.is_valid():
+            coach = serializer.save()
+            coach.save()
+            return JsonResponse(CoachSerializer(coach).data)
+        else:
+            data['errors'] = serializer.errors
+            return JsonResponse(data)
 
     def put(self, request, *args, **kwargs):
         pass
